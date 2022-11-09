6ff68f65a887124d03a17f6ff7a045ff5de25788
Guirdo <produccionesaldo@gmail.com>
2022-11-09 09:32:42 -0600

    FEATURE: Add multiple nested pages building and remove hash from assets

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
-import { resolve } from 'path'
+import glob from 'glob'
+import { resolve, relative, extname, basename, sep } from 'path'
 import { defineConfig } from 'vite'
 
 const root = resolve(__dirname, 'pages')
 const outDir = resolve(__dirname, 'dist')
-const publicDir = resolve(__dirname,'public')
+const publicDir = resolve(__dirname, 'public')
+
+const input = Object.fromEntries(glob.sync('pages/**/*.html').map(file => {
+    const fileName = resolve(root, relative('pages', file))
+    let entryFile = relative('pages', file.slice(0, file.length - basename(file).length))
+    let filePathSep = entryFile.split(sep)
+
+    //When it comes to build a post file
+    if (filePathSep.find(e => e === 'post')) {
+        entryFile = basename(file).substring(0, basename(file).length - extname(file).length)
+        return [entryFile, fileName]
+    }
+    //The main file
+    if (filePathSep[0] === '') {
+        return ['main', fileName]
+    }
+
+    return [entryFile, fileName]
+}))
+
+console.log(input)
 
 export default defineConfig({
     root,
@@ -12,10 +33,12 @@ export default defineConfig({
         outDir,
         emptyOutDir: true,
         rollupOptions: {
-            input: {
-                main: resolve(root, 'index.html'),
-                home: resolve(root, 'home', 'index.html'),
-                guestbook: resolve(root, 'guestbook', 'index.html'),
+            input,
+            output: {
+                //Removing hash from output files
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
             }
         }
     }

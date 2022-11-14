cd158e3d774f4b58d7d003ae983d4ca267a41618
Guirdo <produccionesaldo@gmail.com>
2022-11-13 19:43:05 -0600

    FEATURE: Add post list in Blog page and publish the first post

--- pages/blog/main.js
+++ pages/blog/main.js
@@ -1,1 +1,15 @@
 import '../../styles/global.css'
+import postDB from '../../data/posts.json' assert {type: 'json'}
+
+const posts = postDB.posts
+
+const blogList = document.querySelector('.blog-list')
+
+blogList.innerHTML = posts.map(p => (`
+<li class="blog__item">
+    <a class="blog__link" href="/blog/post/${p.slug}.html">
+        ${p.title}
+    </a>
+    <span class="blog__date">${p.date}</span>
+</li>
+`))
